da245602ebe34859f901e1bcb24608bce0ca4973
Rolfe Schmidt <rolfeschmidt@gmail.com>
2022-01-10 16:44:01 -0700

    add an app header

--- signal-protocol-client/src/App.tsx
+++ signal-protocol-client/src/App.tsx
@@ -11,7 +11,16 @@ function App() {
     const username = useObservable(usernameSubject, '')
     const currSession = useObservable(currentSessionSubject, null)
     return (
-        <div className="App">{username ? currSession ? <SessionDetails /> : <SessionList /> : <CreateIdentity />}</div>
+        <div className="App"><h1>Signal Protocol Chat</h1>
+        <p>
+            <em>A demonstration of the <a href="https://github.com/privacyresearchgroup/libsignal-protocol-typescript">Signal Protocol Typescript SDK</a>. 
+            Source code available <a href="https://github.com/privacyresearchgroup/libsignal-typescript-demo">here</a>.
+            </em>
+        </p>
+        <hr />
+        {
+        username ? currSession ? <SessionDetails /> : <SessionList /> : <CreateIdentity />}
+        </div>
     )
 }
 
